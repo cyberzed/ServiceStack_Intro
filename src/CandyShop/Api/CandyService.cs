fca29f2305b04b20b2c941f6496999fa4ff99f2a
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-01-10 16:00:40 +0100

    Added more implementations to CandyService

--- src/CandyShop/Api/CandyService.cs
+++ src/CandyShop/Api/CandyService.cs
@@ -1,10 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using CandyStack.DTO;
 using CandyStack.Domain;
 using ServiceStack.Common.Web;
-using ServiceStack.ServiceInterface;
 using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
 
 namespace CandyStack.Api
 {
@@ -31,7 +33,64 @@ namespace CandyStack.Api
 				return candies;
 			}
 
+			if (request.MinPrice.HasValue || request.MaxPrice.HasValue)
+			{
+				return SearchByPrice(request.MinPrice, request.MaxPrice);
+			}
+
 			return Db.Select<Candy>();
+		}
+
+		public object Post(Candy request)
+		{
+			if (request.Id != default(uint))
+			{
+				return new HttpResult(HttpStatusCode.Conflict);
+			}
+
+			Db.Insert(request);
+
+			var id = Db.GetLastInsertId();
+
+			request.Id = Convert.ToUInt16(id);
+
+			return request;
+		}
+
+		public object Put(Candy request)
+		{
+			if (request.Id == default(uint))
+			{
+				return new HttpResult(HttpStatusCode.BadRequest);
+			}
+
+			Db.Save(request);
+
+			return request;
+		}
+
+		public object Delete(Candy request)
+		{
+			if (request.Id == default(uint))
+			{
+				return new HttpResult(HttpStatusCode.BadRequest);
+			}
+
+			Db.DeleteById<Candy>(request.Id);
+
+			return new HttpResult(HttpStatusCode.OK);
+		}
+
+		private List<Candy> SearchByPrice(decimal? minPrice, decimal? maxPrice)
+		{
+			if (minPrice.HasValue && maxPrice.HasValue)
+			{
+				return Db.Select<Candy>(c => c.Price >= minPrice.Value && c.Price <= maxPrice.Value);
+			}
+
+			return minPrice.HasValue
+				       ? Db.Select<Candy>(c => c.Price >= minPrice.Value)
+				       : Db.Select<Candy>(c => c.Price <= maxPrice.Value);
 		}
 	}
 }
