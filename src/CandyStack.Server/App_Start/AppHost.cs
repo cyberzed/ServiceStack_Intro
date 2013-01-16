7616e853019ef3a1e1f201126ac3cdede9d085c4
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-01-16 14:36:39 +0100

    Added NLog

--- src/CandyStack.Server/App_Start/AppHost.cs
+++ src/CandyStack.Server/App_Start/AppHost.cs
@@ -1,8 +1,6 @@
-using System.Diagnostics;
 using CandyStack.Server.App_Start;
 using CandyStack.Server.Installers;
-using ServiceStack.Common;
-using ServiceStack.ServiceHost;
+using ServiceStack.Logging.NLogger;
 using ServiceStack.WebHost.Endpoints;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof (AppHost), "Start")]
@@ -13,6 +11,7 @@ namespace CandyStack.Server.App_Start
 	{
 		public AppHost() : base("CandyStack", typeof (AppHost).Assembly)
 		{
+			Config.LogFactory = new NLogFactory();
 		}
 
 		public override void Configure(Funq.Container container)
@@ -21,12 +20,12 @@ namespace CandyStack.Server.App_Start
 
 			SetConfig(new EndpointHostConfig
 				{
-					EnableFeatures = Feature.All.Remove(Feature.Xml).Remove(Feature.Soap)
+					//EnableFeatures = Feature.All.Remove(Feature.Xml).Remove(Feature.Soap)
 				});
 
-			RequestFilters.Add((req, res, obj) => { Debug.WriteLine(req.AbsoluteUri); });
+			//RequestFilters.Add((req, res, obj) => { Debug.WriteLine(req.AbsoluteUri); });
 
-			CandyFormat.Register(this);
+			//CandyFormat.Register(this);
 
 			var installers = new IFunqInstaller[] {new OrmLiteInstaller(), new InfrastructureInstaller()};
 
