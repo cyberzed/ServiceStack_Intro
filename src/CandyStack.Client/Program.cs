9a2d12e17429cfc3db8fcc4b033ece6b280a2d6c
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-01-16 12:43:25 +0100

    Added minimal client to show off how easy it is to use

---
+++ src/CandyStack.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CandyStack.Models.DTO;
+using ServiceStack.ServiceClient.Web;
+
+namespace CandyStack.Client
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var serverUrl = "http://localhost:57441/";
+
+			var client = new JsonServiceClient(serverUrl);
+
+			var isDatabaseSetupForDemo = client.Get(new Payload());
+
+			Console.WriteLine("IsDatabaseSetupForDemo: {0}", isDatabaseSetupForDemo);
+
+			var candies = client.Get(new CandyRequest());
+		}
+	}
+}
--- src/CandyStack.Models/DTO/Payload.cs
+++ src/CandyStack.Models/DTO/Payload.cs
@@ -3,7 +3,7 @@ using ServiceStack.ServiceHost;
 namespace CandyStack.Models.DTO
 {
 	[Route("/cargo")]
-	public class Payload
+	public class Payload : IReturn<bool>
 	{
 		public bool IsShopEmpty { get; set; }
 	}
