f2dfc241b08be3d088d242eca7e2e295230c4657
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-02-13 09:41:35 +0100

    Added localhost restriction to cargoservice

--- src/CandyStack.Server/Api/CargoService.cs
+++ src/CandyStack.Server/Api/CargoService.cs
@@ -1,9 +1,11 @@
 using CandyStack.Models.DTO;
 using CandyStack.Server.Services;
+using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 
 namespace CandyStack.Server.Api
 {
+	[Restrict(LocalhostOnly = true)]
 	public class CargoService : Service
 	{
 		private readonly DbManagementService dbManagementService;
