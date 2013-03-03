b03f4b8d05e72555c72bf1e6f26379210fe35ef9
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-03 12:13:44 +0100

    Fixed a wrong step in miniprofiler registration in global.asax

--- src/CandyStack.Server/Global.asax.cs
+++ src/CandyStack.Server/Global.asax.cs
@@ -16,14 +16,16 @@ namespace CandyStack.Server
 		protected void Application_BeginRequest(object sender, EventArgs e)
 		{
 			if (Request.IsLocal)
-			{
 				Profiler.Start();
-			}
+		}
+
+		protected void Application_EndRequest(object src, EventArgs e)
+		{
+			Profiler.Stop();
 		}
 
 		protected void Application_AuthenticateRequest(object sender, EventArgs e)
 		{
-			Profiler.Stop();
 		}
 
 		protected void Application_Error(object sender, EventArgs e)
