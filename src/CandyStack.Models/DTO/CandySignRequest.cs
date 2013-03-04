23e4b4e6c5b01020b5ecf1f578ad9bc4233bb817
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-04 14:08:11 +0100

    Added more routes to CandySign creation

--- src/CandyStack.Models/DTO/CandySignRequest.cs
+++ src/CandyStack.Models/DTO/CandySignRequest.cs
@@ -2,7 +2,9 @@ using ServiceStack.ServiceHost;
 
 namespace CandyStack.Models.DTO
 {
+	[Route("/candysign", "GET")]
 	[Route("/candysign/{Id}", "GET")]
+	[Route("/candysign/{Id}/{Width}/{Height}", "GET")]
 	public class CandySignRequest
 	{
 		public uint Id { get; set; }
